a29eaf2b6fa7f275a075e6ec539c07a0e6dd134f
eduardo.fiorentin <eduardoviniciusfiorentin@gmail.com>
2026-02-14 21:14:34 -0300

    back-add: sql script to see columns in database

--- backend/sql/util_scripts.sql
+++ backend/sql/util_scripts.sql
@@ -43,8 +43,19 @@ drop procedure proc_delete_user_and_roles;
 
 
 
-
-
+-- Ver informações de uma coluna 
+SELECT 
+    column_name, 
+    data_type, 
+    is_nullable, 
+    column_default,
+    character_maximum_length
+FROM 
+    information_schema.columns
+WHERE 
+    table_schema = 'public'
+    AND table_name = 'users'
+    AND column_name = 'password';
 
 
 
