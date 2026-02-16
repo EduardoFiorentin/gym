5e2d53d105d25b1f52b9997345eb6e02d09870be
eduardo.fiorentin <eduardoviniciusfiorentin@gmail.com>
2026-02-16 14:30:59 -0300

    infra-rm: comments from deploy file

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,12 @@
 
 echo "Build process started..."
 
-# 1. Para e remove containers, redes e volumes órfãos definidos no compose
 echo "Removing old containers and networks..."
 docker compose down --remove-orphans
 
-# 2. Limpa imagens 'dangling' (camadas inúteis que ficam após o rebuild)
 echo "Removing old images..."
 docker image prune -f
 
-# 3. Sobe o ambiente construindo a imagem do Spring novamente
 echo "Building applications..."
 # docker-compose up --build --no-cache -d
 docker compose build --no-cache
