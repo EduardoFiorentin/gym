2913ca734542b2b0e66a6871bc49a5b42c4e8fbb
eduardo.fiorentin <eduardoviniciusfiorentin@gmail.com>
2026-03-02 00:01:20 -0300

    infra-feat: add db_0.0.1 version - standardizes stored dates to UTC 0

---
+++ backend/sql/database/drop_db_0.0.1.sql
@@ -0,0 +1,17 @@
+truncate table public.serie CASCADE;
+truncate table public.treinamento CASCADE;
+truncate table public.exercicio CASCADE;
+truncate table public.un_medida CASCADE;
+truncate table public.treino CASCADE;
+TRUNCATE TABLE public.users_roles CASCADE;
+TRUNCATE TABLE public.users CASCADE;
+truncate TABLE public.roles CASCADE;
+
+drop table public.serie;
+drop table public.treinamento;
+drop table public.exercicio;
+drop table public.un_medida;
+drop table public.treino;
+drop TABLE public.users_roles;
+drop TABLE public.users;
+drop TABLE public.roles;
